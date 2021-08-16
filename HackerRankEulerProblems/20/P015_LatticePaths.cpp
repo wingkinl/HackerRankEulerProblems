1fc18ff1855c2a0fcedd29f7b93d363642d0338f
wingkinl <yonken@gmail.com>
2021-08-16 10:47:42 +0800

    Update P015_LatticePaths.cpp

--- HackerRankEulerProblems/20/P015_LatticePaths.cpp
+++ HackerRankEulerProblems/20/P015_LatticePaths.cpp
@@ -2,6 +2,25 @@
 #include <iostream>
 #include <vector>
 
+// For a given grid, we can start from the bottom right corner
+// and try to find routes back to every other point, and remember
+// the number of routes at each point.
+// There is exactly only 1 route to reach every point that locates
+// on the right and bottom "edge", due to the fact that we can only
+// move left and up (the opposite direction when start from top left).
+//
+// Here's the routes for a 2x2 grid:
+//   0 0 1
+//   0 0 1
+//   1 1 X
+// 
+// From the bottom up, the number of routes equals to the number of 
+// routes of its right and down point. Eventually we can get the result.
+// 
+//   6 3 1
+//   3 2 1
+//   1 1 X
+//
 unsigned int P015LatticePaths::Solve(int n, int m)
 {
 	std::vector<uint64_t> routes(n + 1, 1);
